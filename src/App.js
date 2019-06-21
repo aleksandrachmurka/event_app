f1d7f775f00d2f97f9003a177b3ec445193d8d91
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-06-21 22:04:30 +0200

    Create EventsList component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EventsList from './components/EventsList'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app__header">
+        <h1>Events</h1>
       </header>
+      <main className="app__main">
+        <EventsList />
+      </main>
     </div>
   );
 }
