351d00cfd284815d600cff31373f2d3bc6e0821d
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-08-03 19:00:53 +0200

    Add more descriptions in event component

--- src/components/Event.js
+++ src/components/Event.js
@@ -8,9 +8,9 @@ const Event = ({ ...props }) => {
         <div>
             <h3>{event.title}</h3>
             <p>{event.category}</p>
-            <address>{event.location}</address>
-            <time>{`${event.date} ${event.time} `}</time>
-            <p>{event.organizer}</p>
+            <address>{`Will be held at: ${event.location}`}</address>
+            <time>{`${event.date} ${event.time}`}</time>
+            <p>{`Organizer: ${event.organizer}`}</p>
             <p>{event.description}</p>
             <button onClick={()=> removeEvent(event.key)}>Remove event</button>
         </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -17,39 +17,42 @@ const Form = ({ ...props }) => {
     }
     
     return (
-        <form onSubmit={handleSubmit}>
-            <label>What is Event title</label>
-            <input type="text" name="title"
-            placeholder="Add event title" 
-            onChange={handleInput}
-            required
-            />
+        <>
+            <p>Add event</p>
+            <form onSubmit={handleSubmit}>
+                <label>What is Event title</label>
+                <input type="text" name="title"
+                placeholder="Add event title" 
+                onChange={handleInput}
+                required
+                />
 
-            <label>When will it take place?</label>
-            <input type="date"  onChange={handleInput} name="date"/>
-            <input type="time" onChange={handleInput} name="time" />
+                <label>When will it take place?</label>
+                <input type="date"  onChange={handleInput} name="date"/>
+                <input type="time" onChange={handleInput} name="time" />
 
-            <label>Add Event description</label>
-            <textarea onChange={handleInput} name="description" type="text" rows="5" cols="10" maxLength="360" />
+                <label>Add Event description</label>
+                <textarea onChange={handleInput} name="description" type="text" rows="5" cols="10" maxLength="360" />
 
-            <label>Event Organizer</label>
-            <input type="text" name="organizer"
-            placeholder="Add events organizer" 
-            onChange={handleInput}
-            required
-            />
+                <label>Event Organizer</label>
+                <input type="text" name="organizer"
+                placeholder="Add events organizer" 
+                onChange={handleInput}
+                required
+                />
 
-            <label>Choose Event category</label>
-            <select onChange={handleInput} name="category" required>
-                <option value="" disabled selected>Please select category</option>
-                <option value="music">Music</option>
-                <option value="theatre">Theatre</option>
-                <option value="meeting">Meeting</option>
-                <option value="exhibition">Exhibition</option>
-            </select>
+                <label>Choose Event category</label>
+                <select onChange={handleInput} name="category" required>
+                    <option value="" disabled selected>Please select category</option>
+                    <option value="music">Music</option>
+                    <option value="theatre">Theatre</option>
+                    <option value="meeting">Meeting</option>
+                    <option value="exhibition">Exhibition</option>
+                </select>
 
-            <button type="submit"> Add Event </button>
-        </form>
+                <button type="submit"> Add Event </button>
+            </form>
+        </>
     )
 }
 
